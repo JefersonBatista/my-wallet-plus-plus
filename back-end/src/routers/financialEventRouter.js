5614e30e1a2c95ce4c38cdf9cc1b87f3a2b52241
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-04-12 12:27:04 -0300

    Add controllers

--- back-end/src/routers/financialEventRouter.js
+++ back-end/src/routers/financialEventRouter.js
@@ -1,116 +1,16 @@
-import jwt from "jsonwebtoken";
-import connection from "../database.js";
-
 import { Router } from "express";
+
+import * as financialEventController from "../controllers/financialEventController.js";
 
 const financialEventRouter = Router();
 
-financialEventRouter.post("/financial-events", async (req, res) => {
-	try {
-		const authorization = req.headers.authorization || "";
-		const token = authorization.replace("Bearer ", "");
+financialEventRouter.post(
+	"/financial-events",
+	financialEventController.register
+);
 
-		if (!token) {
-			return res.sendStatus(401);
-		}
+financialEventRouter.get("/financial-events", financialEventController.list);
 
-		let user;
-
-		try {
-			user = jwt.verify(token, process.env.JWT_SECRET);
-		} catch {
-			return res.sendStatus(401);
-		}
-
-		const { value, type } = req.body;
-
-		if (!value || !type) {
-			return res.sendStatus(422);
-		}
-
-		const financialTypes = ["INCOME", "OUTCOME"];
-		if (!financialTypes.includes(type)) {
-			return res.sendStatus(422);
-		}
-
-		if (value < 0) {
-			return res.sendStatus(422);
-		}
-
-		await connection.query(
-			`INSERT INTO "financialEvents" ("userId", "value", "type") VALUES ($1, $2, $3)`,
-			[user.id, value, type]
-		);
-
-		res.sendStatus(201);
-	} catch (err) {
-		console.error(err);
-		res.sendStatus(500);
-	}
-});
-
-financialEventRouter.get("/financial-events", async (req, res) => {
-	try {
-		const authorization = req.headers.authorization || "";
-		const token = authorization.replace("Bearer ", "");
-
-		if (!token) {
-			return res.sendStatus(401);
-		}
-
-		let user;
-
-		try {
-			user = jwt.verify(token, process.env.JWT_SECRET);
-		} catch {
-			return res.sendStatus(401);
-		}
-
-		const events = await connection.query(
-			`SELECT * FROM "financialEvents" WHERE "userId"=$1 ORDER BY "id" DESC`,
-			[user.id]
-		);
-
-		res.send(events.rows);
-	} catch (err) {
-		console.error(err);
-		res.sendStatus(500);
-	}
-});
-
-financialEventRouter.get("/financial-events/sum", async (req, res) => {
-	try {
-		const authorization = req.headers.authorization || "";
-		const token = authorization.replace("Bearer ", "");
-
-		if (!token) {
-			return res.sendStatus(401);
-		}
-
-		let user;
-
-		try {
-			user = jwt.verify(token, process.env.JWT_SECRET);
-		} catch {
-			return res.sendStatus(401);
-		}
-
-		const events = await connection.query(
-			`SELECT * FROM "financialEvents" WHERE "userId"=$1 ORDER BY "id" DESC`,
-			[user.id]
-		);
-
-		const sum = events.rows.reduce(
-			(total, event) =>
-				event.type === "INCOME" ? total + event.value : total - event.value,
-			0
-		);
-
-		res.send({ sum });
-	} catch (err) {
-		console.error(err);
-		res.sendStatus(500);
-	}
-});
+financialEventRouter.get("/financial-events/sum", financialEventController.sum);
 
 export default financialEventRouter;
